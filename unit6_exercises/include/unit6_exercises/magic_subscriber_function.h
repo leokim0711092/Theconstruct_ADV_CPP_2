b16261bb8331d3f6031b1ffc9aa8af093d39d174
leokim0711092 <leokim10073@gmail.com>
2023-09-19 11:35:30 +0000

    Unit 6: Exercise 6.1 Class and Function template

---
+++ unit6_exercises/include/unit6_exercises/magic_subscriber_function.h
@@ -0,0 +1,38 @@
+#ifndef MAGIC_SUBSCRIBER_H
+#define MAGIC_SUBSCRIBER_H
+
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+
+using namespace std;
+
+class MagicSubscriber {
+public:
+  MagicSubscriber(){
+    cout << "MagicSubscriber Constructor is called" << endl;
+  }
+  ~MagicSubscriber(){ cout << "MagicSubscriber Destructor is called" << endl;}
+
+  template<typename RosNT>
+  void init(ros::NodeHandle &ros_node, const string subscriber_topic){
+    this->m_ros_node_object = &ros_node;
+    this->m_subscriber_topic = subscriber_topic;
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,ros::console::levels::Info)) {
+      ros::console::notifyLoggerLevelsChanged();
+    }
+    image_sub_ = this->m_ros_node_object->subscribe(this->m_subscriber_topic,1,&MagicSubscriber::CallbackToTopic<RosNT>,this);}
+
+  template<typename RosNT>
+  void CallbackToTopic(const typename RosNT::ConstPtr &msg){
+  // the uint8 is an alias of unsigned char, therefore needs casting to int
+    ROS_INFO_STREAM(
+        "Call Back Topic Image Data[0]=" << static_cast<int>(msg->data[0]));
+  }
+
+private:
+  ros::NodeHandle *m_ros_node_object;
+  string m_subscriber_topic;
+  ros::Subscriber image_sub_;
+};
+
+#endif
