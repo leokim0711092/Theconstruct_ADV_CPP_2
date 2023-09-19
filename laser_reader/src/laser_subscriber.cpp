04376bf288df1436a652c4dc62b91c9db3f1e706
leokim0711092 <leokim10073@gmail.com>
2023-09-19 03:36:24 +0000

    Unit 5: Exercise 5.9 Const reference

--- laser_reader/src/laser_subscriber.cpp
+++ laser_reader/src/laser_subscriber.cpp
@@ -11,14 +11,28 @@ class LaserSubscriberNode{
             laser_sub = nh.subscribe("kobuki/laser/scan",10, &LaserSubscriberNode::laser_subscribe_callback, this);
         
         }
+        void printClosestObstacleDistance(){
+            float min_range = laser_scan -> ranges[0];
+            int rc;
+            for(int i=0;i<laser_scan->ranges.size();i++){
+                if(laser_scan->ranges[i] < min_range){
+                    min_range = laser_scan->ranges[i];
+                    rc = i;
+                }
+            }
+            float angle = laser_scan->angle_min + rc * laser_scan->angle_increment;
+            // Print the distance to the closest obstacle and the corresponding angle in
+            // radian
+            std::cout << "Closest obstacle distance: " << min_range
+                    << ", Angle (rad): " << angle << std::endl;
+        }
     private:
         ros::Subscriber laser_sub;
         const sensor_msgs::LaserScan *laser_scan; // define the constant pointer laser_scan
         void laser_subscribe_callback(const sensor_msgs::LaserScanConstPtr &msg){
             laser_scan = msg.get();
-            if(!laser_scan->ranges.empty()){
-                float r = laser_scan->ranges[360];
-                ROS_INFO("Range: %f",r);
+            if(!laser_scan->ranges.empty() && laser_scan != nullptr){
+                printClosestObstacleDistance();
             }
         }
 };
